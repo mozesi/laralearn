6cb5954001d89fe1887d6d186aa1541e8fa6c615
mozesi <msukwamose1@gmail.com>
2023-04-23 15:01:21 +0200

    explored laravel routing

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index(){
+        return "Hello  world";
+    }
+
+    public function store(){
+        return "Hello  world";
+    }
+
+    public function create(){
+        return "Hello  world";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
+use \App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
+use \App\Models\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*
+use of regex to match route parameters. 
+this further helps in securing the application
+*/
+Route::get('/home/{id?}/name/{name}', function ($id = 3,$name) {
+    return $name;
+}) ->where('id','[0-9]+')->name('home.page');
+
+/*
+naming routes for easy reference in any part of the 
+application. can use url('/home/page') function or naming the route as below.
+naming is easier  for complex routes and linking by name is easier.
+*/
+Route::get('/home/page', function () {
     return view('welcome');
+}) ->name('home.page');
+
+/**
+ * Route grouping
+ * group routes with common characteristics like
+ * --authentication, path prefix, controller name space
+ * it less tedious, visual cues to future developers, apply shared configurations settings
+ * 
+ */
+
+ Route::prefix('tasks')->group(function(){
+    Route::get('home', [TasksController::class,'index'])->name('tasks.all');
+    Route::get('create', [TasksController::class,'create'])->name('tasks.create'); 
+    Route::post('store', [TasksController::class,'store'])->name('tasks.store');
+});
+
+/**
+ * Model binding
+ */
+
+Route::get('user/{user}',function(User $user){
+    return $user;
 });
